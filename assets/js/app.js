4f620ad4988b79e781faf4e9c77927212b6b365f
arminsafie <arminsafaie92@gmail.com>
2024-02-10 11:20:24 +0330

    final

--- assets/js/app.js
+++ assets/js/app.js
@@ -18,8 +18,10 @@ const renderMoive = (filter = "") => {
   filterdMovies.forEach((movie) => {
     const movieEl = document.createElement("li");
     const { info, ...otherProps } = movie;
-    const { title: newTitel } = info;
-    let text = newTitel + "-";
+    // const { title: newTitel } = info;
+    let { getFormattedTitle } = info;
+
+    let text = movie.getFormattedTitle() + "-";
     for (const key in info) {
       if (key !== "title") {
         text = text + `${key}: ${info[key]}`;
@@ -44,11 +46,26 @@ const addMovieHandeler = () => {
   }
   const newMovie = {
     info: {
-      title,
+      set title(val) {
+        if (val.trim() === "") {
+          this._title = "DEFAULT";
+          return;
+        }
+        this._title = val;
+      },
+      get title() {
+        return this._title;
+      },
       [extraName]: extraValue,
     },
+
     id: Math.random().toString(),
+    getFormattedTitle() {
+      return this.info.title.toUpperCase();
+    },
   };
+  newMovie.info.title = title;
+  console.log(newMovie.info.title);
   movies.push(newMovie);
   renderMoive();
 };
