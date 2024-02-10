cb056938f65f5fa9a8c2cb824dea44d3080f7467
arminsafie <arminsafaie92@gmail.com>
2024-02-10 10:04:01 +0330

    render Movie

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,6 +1,23 @@
 const addMovieBtn = document.getElementById("add-movie-btn");
 const searchBtn = document.getElementById("search-btn");
 const movies = [];
+
+const renderMoive = () => {
+  const movieList = document.getElementById("movie-list");
+  if (movies.length === 0) {
+    movieList.classList.remove("visible");
+    return;
+  } else {
+    movieList.classList.add("visible");
+  }
+  movieList.innerHTML = "";
+  movies.forEach((movie) => {
+    const movieEl = document.createElement("li");
+    movieEl.textContent = movie.info.title;
+    movieList.append(movieEl);
+  });
+};
+
 const addMovieHandeler = () => {
   const title = document.getElementById("title").value;
   const extraName = document.getElementById("extra-name").value;
@@ -21,7 +38,7 @@ const addMovieHandeler = () => {
     id: Math.random(),
   };
   movies.push(newMovie);
-  console.log(movies);
+  renderMoive();
 };
 
 addMovieBtn.addEventListener("click", addMovieHandeler);
